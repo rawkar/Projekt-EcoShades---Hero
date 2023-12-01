a68b359351822343ffac19e49a31fdd00a2c5bc2
’rawkar’ <rawaz.k@hotmail.com>
2023-12-01 15:58:48 +0100

    add overlay and BG-fix

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,44 @@
-
-    var swiper = new Swiper('.swiper', {
-      slidesPerView: 3,
-      direction: getDirection(),
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
       },
-      on: {
-        resize: function () {
-          swiper.changeDirection(getDirection());
-        },
-      },
+    },
 
-      pagination: {
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
-    });
 
-    function getDirection() {
-      var windowWidth = window.innerWidth;
-      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-      return direction;
-    }
-
-
+    //   breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     1240: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 40
+    //     }
+    //   }
     
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
