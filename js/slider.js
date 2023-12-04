d5f37c43337ce0dd261c2e604f434d74ef3dc12f
’rawkar’ <rawaz.k@hotmail.com>
2023-12-04 14:01:22 +0100

    add styling to swiper

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 50,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -13,15 +13,15 @@ var swiper = new Swiper(".mySwiper", {
     breakpoints: {
       640: {
         slidesPerView: 1,
-        spaceBetween: 20,
+        spaceBetween: 50,
       },
       768: {
         slidesPerView: 2,
-        spaceBetween: 10,
+        spaceBetween: 30,
       },
       1324: {
         slidesPerView: 3,
-        spaceBetween: 10,
+        spaceBetween: 50,
       },
     },
   });
