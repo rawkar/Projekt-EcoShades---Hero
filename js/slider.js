39e8c751e6a8f5669f15ca834c1df903b3029dbf
’rawkar’ <rawaz.k@hotmail.com>
2023-12-04 13:30:46 +0100

    add fix to swiper

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,27 @@
-
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1324: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
     },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1400: {
-      slidesPerView: 3  ,
-      spaceBetween: 30,
-    },
-  },
-});
+  });
